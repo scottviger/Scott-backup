d64cebebf67f2f4015a3fc2760d2d07f853a2025
scottviger <301scottviger301@gmail.com>
2017-02-17 14:22:28 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -30,7 +30,7 @@ DIR=\`pwd\`
 cd /
 mysql -u $mysqluser -p$mysqlpass $mysql_sql < $DIR/$mysql_sql.sql
 sudo rm -R $src_dir
-sudo tar -Jxvf "$DIR/data.tar.xz"
+sudo tar -Jxvf $DIR/data.tar.xz
 """ >> $des_dir/$dirname/restore.sh
 
 echo "Unmounting"
