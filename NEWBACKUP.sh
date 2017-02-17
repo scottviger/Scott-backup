6b14980f1a1f1eb9d6a06560cfee629de2054a85
scottviger <301scottviger301@gmail.com>
2017-02-16 21:01:11 -0500

    Update NEWBACKUP.sh

--- NEWBACKUP.sh
+++ NEWBACKUP.sh
@@ -14,10 +14,12 @@ des_dir=$5	#Input capture DON'T FINISH WITH /
 echo "Mounting" #Mount backup dir (Optional)
 mount /dev/sda1 $des_dir
 cd /
+
 echo "Saving." #start saving process
 mkdir $des_dir/$dirname && echo "."
 mysqldump --opt -u $mysqluser -p$mysqlpass $mysql_sql > $des_dir/$dirname/$mysql_sql.sql && echo "."
 XZ_OPT=-9 tar -cpJf $des_dir/$dirname/data.tar.xz $src_dir && echo ".Done!"
+
 echo "Creating Restoration script"
 echo """
 #!/bin/bash
@@ -29,6 +31,7 @@ mysql -u $mysqluser -p$mysqlpass $mysql_sql < $des_dir/$dirname/$mysql_sql.sql
 rm -R $src_dir
 sudo tar -Jxvf "$des_dir/$dirname/data.tar.xz"
 """ >> $des_dir/$dirname/restore.sh
+
 echo "Unmounting"
 umount $des_dir
 echo "Don't forget,"
