4f64e8afcbc0cfd33a5caa0a50f7bf24c72c99e3
scottviger <301scottviger301@gmail.com>
2017-02-18 10:13:38 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -30,9 +30,8 @@ cd /
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" ' >> $des_dir/$dirname/restore.sh
 
 echo "mysql -u $mysqluser -p$mysqlpass $mysql_sql < \$DIR/$mysql_sql.sql
-sudo rm -R $src_dir" >> $des_dir/$dirname/restore.sh
-
-echo 'sudo tar -Jxvf $DIR/data.tar.xz' >> $des_dir/$dirname/restore.sh
+sudo rm -R $src_dir
+sudo tar -Jxvf \$DIR/data.tar.xz" >> $des_dir/$dirname/restore.sh
 
 echo "Unmounting"
 umount /backup
